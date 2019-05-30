a6d6aab5b4d7e0e7f9235d2832399e1a65bfeab5
fr42k <fr42k@yahoo.com>
2019-05-29 21:08:27 -0500

    update at 2019-05-29

---
+++ solutions/066-plus-one/plus-one.py
@@ -0,0 +1,45 @@
+# Given a non-empty array of digits representing a non-negative integer, plus one to the integer.
+#
+# The digits are stored such that the most significant digit is at the head of the list, and each element in the array contain a single digit.
+#
+# You may assume the integer does not contain any leading zero, except the number 0 itself.
+#
+# Example 1:
+#
+#
+# Input: [1,2,3]
+# Output: [1,2,4]
+# Explanation: The array represents the integer 123.
+#
+#
+# Example 2:
+#
+#
+# Input: [4,3,2,1]
+# Output: [4,3,2,2]
+# Explanation: The array represents the integer 4321.
+#
+
+
+class Solution:
+    def plusOne(self, digits: List[int]) -> List[int]:
+        carry = 0
+        for i in range(len(digits)-1,-1,-1):
+            digit = digits[i]
+            if (carry == 1):
+                if (digit == 9):
+                    digits[i] = 0
+                    continue
+                else:
+                    digits[i] += 1
+                    carry = 0
+            else:
+                if (i == len(digits)-1):
+                    if (digit == 9):
+                        digits[i] = 0
+                        carry = 1
+                    else:
+                        digits[i] += 1
+        if (carry == 1):
+            return [1]+digits
+        return digits
---
+++ solutions/169-majority-element/majority-element.py
@@ -0,0 +1,34 @@
+# Given an array of size n, find the majority element. The majority element is the element that appears more than ⌊ n/2 ⌋ times.
+#
+# You may assume that the array is non-empty and the majority element always exist in the array.
+#
+# Example 1:
+#
+#
+# Input: [3,2,3]
+# Output: 3
+#
+# Example 2:
+#
+#
+# Input: [2,2,1,1,1,2,2]
+# Output: 2
+#
+#
+
+
+class Solution:
+    def majorityElement(self, nums: List[int]) -> int:
+        frequencies = dict()
+        # Construct and initialize the number frequency dictionary
+        for n in nums:
+            frequencies[n] = 0
+        # Count frequencies for each number
+        for n in nums:
+            frequencies[n] += 1
+        max_freq = max(frequencies.values())
+        for num in frequencies:
+            if (frequencies[num] == max_freq):
+                return num
+        return -1
+        
