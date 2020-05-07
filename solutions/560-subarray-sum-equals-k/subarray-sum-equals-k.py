c7be92ceebd343e795a078291c5a9135edd95c52
fr42k <fr42k@yahoo.com>
2020-05-06 21:09:58 -0700

    update at 2020-05-06

--- solutions/560-subarray-sum-equals-k/subarray-sum-equals-k.py
+++ solutions/560-subarray-sum-equals-k/subarray-sum-equals-k.py
@@ -2,16 +2,17 @@
 #
 # Example 1:
 #
+#
 # Input:nums = [1,1,1], k = 2
 # Output: 2
 #
 #
+#  
+# Constraints:
 #
-# Note:
 #
-# The length of the array is in range [1, 20,000].
-# The range of numbers in the array is [-1000, 1000] and the range of the integer k is [-1e7, 1e7].
-#
+# 	The length of the array is in range [1, 20,000].
+# 	The range of numbers in the array is [-1000, 1000] and the range of the integer k is [-1e7, 1e7].
 #
 #
 
