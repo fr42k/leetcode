2423c714721c2bc8d54f5aacdbbc09424a35b269
fr42k <fr42k@yahoo.com>
2020-05-15 19:57:11 -0700

    update at 2020-05-15

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # :pencil2: Leetcode Solutions with Cpp,Python,Python3  
-Update time:  2020-05-15 13:48:51  
+Update time:  2020-05-15 19:57:10  
 Auto created by [leetcode_generate](https://github.com/bonfy/leetcode)  
 I have solved **21   /   1322** problems  
 while there are **212** problems still locked.  
--- solutions/1039-find-the-town-judge/find-the-town-judge.py
+++ solutions/1039-find-the-town-judge/find-the-town-judge.py
@@ -13,56 +13,29 @@
 # If the town judge exists and can be identified, return the label of the town judge.  Otherwise, return -1.
 #
 #  
-#
 # Example 1:
-#
-#
 # Input: N = 2, trust = [[1,2]]
 # Output: 2
-#
-#
-#
 # Example 2:
-#
-#
 # Input: N = 3, trust = [[1,3],[2,3]]
 # Output: 3
-#
-#
-#
 # Example 3:
-#
-#
 # Input: N = 3, trust = [[1,3],[2,3],[3,1]]
 # Output: -1
-#
-#
-#
 # Example 4:
-#
-#
 # Input: N = 3, trust = [[1,2],[2,3]]
 # Output: -1
-#
-#
-#
 # Example 5:
-#
-#
 # Input: N = 4, trust = [[1,3],[1,4],[2,3],[2,4],[4,3]]
 # Output: 3
 #
 #  
-#
-#
-#
-#
-#
-# Note:
+# Constraints:
 #
 #
 # 	1 <= N <= 1000
-# 	trust.length <= 10000
+# 	0 <= trust.length <= 10^4
+# 	trust[i].length == 2
 # 	trust[i] are all different
 # 	trust[i][0] != trust[i][1]
 # 	1 <= trust[i][0], trust[i][1] <= N
